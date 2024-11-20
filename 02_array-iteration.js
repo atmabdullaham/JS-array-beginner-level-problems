f8b1492813bd931c513d27a515c9c9b2f89a4b16
atmabdullaham <atmabdullaham@gmail.com>
2024-11-20 19:56:33 +0600

    array iteration, use for loop and use forEach()

--- 02_array-iteration.js
+++ 02_array-iteration.js
@@ -1,6 +1,27 @@
 // Print all elements of an array using a for loop.
-
+const fruits = ["Apple", "Banana", "Orange", "Mango", "Grapes"];
+for (let i = 0; i < fruits.length; i++) {
+ console.log(fruits[i])
+}
 
 
 
 // Use forEach() to log each element.
+
+console.log("Fruits Array using forEach():");
+let newPuhs = [];
+let newUnShift = [];
+fruits.forEach(function (fruit) {
+ console.log(fruit);
+ newUnShift.unshift(fruit);
+ newPuhs.push(fruit);
+
+});
+console.log(newUnShift)
+console.log(newPuhs)
+// Output:
+// Apple
+// Banana
+// Orange
+// Mango
+// Grapes
