fb5d43e14f434ecc243d92c2b054627852fdc10a
atmabdullaham <atmabdullaham@gmail.com>
2024-11-20 11:08:24 +0600

    first commit

---
+++ 01_create-and-access-array.js
@@ -0,0 +1,33 @@
+// 1. Create an array of numbers and access the first and last elements.
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const firstElement = numbers[0];
+console.log(firstElement);
+
+const lastElement = numbers[numbers.length - 1];
+console.log(lastElement);
+
+// 2. Add and remove elements:
+
+const fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+
+const fruitsUpdated = [...fruits];
+console.log(fruitsUpdated)
+
+// there are two ways to add items in an array one is push and another is unShift: 
+//a.  to add items in the end we use "push"
+fruitsUpdated.push("fig"); // Add to the end
+console.log(fruitsUpdated);
+//b.  to add items in the beginning we use "usshift" 
+fruitsUpdated.unshift("grape"); // Add to the beginning
+console.log(fruitsUpdated)
+
+
+// there are two ways to remove items from an array
+//a. to remove items from the end we use "pop"
+fruitsUpdated.pop(); // Remove from the end
+console.log(fruitsUpdated)
+
+//b. to remove items from the beginning we use "shift"
+fruitsUpdated.shift(); // Remove from the beginning
+console.log(fruitsUpdated);
